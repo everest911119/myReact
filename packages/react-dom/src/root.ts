c1795b8d649e05b776009bd9f787dd31395e9a26
everest911119 <everest911119@gmail.com>
2023-01-30 17:00:41 -0500

    feat: review

--- packages/react-dom/src/root.ts
+++ packages/react-dom/src/root.ts
@@ -12,6 +12,7 @@ export function createRoot(container: Container) {
 	const root = createContainer(container);
 	return {
 		render(element: ReactElementType) {
+			// 代理点击事件
 			initEvent(container, 'click');
 			return updateContainer(element, root);
 		}
