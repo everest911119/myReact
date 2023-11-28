2e3741957822e83e95a62f5e64131c93d281a702
everest <everest911119@gmail.com>
2023-11-28 16:40:27 -0500

    test

--- demo/test-fc/mian.tsx
+++ demo/test-fc/mian.tsx
@@ -2,36 +2,36 @@ import { useState, useEffect } from 'react';
 
 import ReactDOM from 'react-dom/client';
 
-function App() {
-	const [num, setNum] = useState(10);
-	const [count, setCount] = useState(0);
-	console.log(num, 'dd');
-	const arr =
-		num % 2 === 0
-			? [
-					<li key="1">1</li>,
-					<li key="2">2</li>,
-					<li key="3">
-						3 <div>123</div>
-					</li>
-			  ]
-			: [<li key="3">3</li>, <li key="2">2</li>, <li key="3">3</li>];
+// function App() {
+// 	const [num, setNum] = useState(10);
+// 	const [count, setCount] = useState(0);
+// 	console.log(num, 'dd');
+// 	const arr =
+// 		num % 2 === 0
+// 			? [
+// 					<li key="1">1</li>,
+// 					<li key="2">2</li>,
+// 					<li key="3">
+// 						3 <div>123</div>
+// 					</li>
+// 			  ]
+// 			: [<li key="3">3</li>, <li key="2">2</li>, <li key="3">3</li>];
 
-	return (
-		<ul
-			onClick={() => {
-				setNum((num) => num + 1);
-				setNum((num) => num + 2);
-				setNum((num) => num + 3);
-				console.log(num, 'in function');
-				// setCount((prev) => prev + 1);
-			}}
-		>
-			<li>{count}</li>
-			<li>{num}</li>
-		</ul>
-	);
-}
+// 	return (
+// 		<ul
+// 			onClick={() => {
+// 				setNum((num) => num + 1);
+// 				setNum((num) => num + 2);
+// 				setNum((num) => num + 3);
+// 				console.log(num, 'in function');
+// 				// setCount((prev) => prev + 1);
+// 			}}
+// 		>
+// 			<li>{count}</li>
+// 			<li>{num}</li>
+// 		</ul>
+// 	);
+// }
 
 // function Child() {
 // 	return (
@@ -41,37 +41,37 @@ function App() {
 // 	);
 // }
 // // console.log(App);
-// function App() {
-// 	const [num, updateNum] = useState(0);
-// 	useEffect(() => {
-// 		console.log('App mount');
-// 		return () => {
-// 			console.log('unmount app');
-// 		};
-// 	}, []);
+function App() {
+	const [num, updateNum] = useState(0);
+	useEffect(() => {
+		console.log('App mount');
+		return () => {
+			console.log('unmount app');
+		};
+	}, []);
 
-// 	useEffect(() => {
-// 		console.log('num change create', num);
-// 		return () => {
-// 			console.log('num change destroy', num);
-// 		};
-// 	}, [num]);
+	useEffect(() => {
+		console.log('num change create', num);
+		return () => {
+			console.log('num change destroy', num);
+		};
+	}, [num]);
 
-// 	return (
-// 		<div onClick={() => updateNum(num + 1)}>
-// 			{num === 0 ? <Child /> : 'noop'}
-// 		</div>
-// 	);
-// }
+	return (
+		<div onClick={() => updateNum(num + 1)}>
+			{num === 0 ? <Child /> : 'noop'}
+		</div>
+	);
+}
 
-// function Child() {
-// 	useEffect(() => {
-// 		console.log('Child mount');
-// 		return () => console.log('Child unmount');
-// 	}, []);
+function Child() {
+	useEffect(() => {
+		console.log('Child mount');
+		return () => console.log('Child unmount');
+	}, []);
 
-// 	return 'i am child';
-// }
+	return 'i am child';
+}
 // function App() {
 // 	const [num, update] = useState(100);
 // 	return (
--- packages/react/node_modules/.ignored_shared/ReactSymbol.ts
+++ packages/react/node_modules/.ignored_shared/ReactSymbol.ts
@@ -4,3 +4,9 @@ const supportSymbol = typeof Symbol === 'function' && Symbol.for;
 export const REACT_ELEMENT_TYPE = supportSymbol
 	? Symbol.for('react.element')
 	: 0xeac7;
+
+// react Fragment type
+
+export const REACT_FRAGMENT_TYPE = supportSymbol
+	? Symbol.for('react.fragement')
+	: 0xeacbb;
